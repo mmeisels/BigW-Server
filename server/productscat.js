4f24153a026bb84cb9aef2d04c17df7dbc27af7c
Michael Meisels <mmeisels@salesforce.com>
2016-06-06 10:18:42 +1000

    Added Product Cat

--- server/productscat.js
+++ server/productscat.js
@@ -3,11 +3,11 @@ var db = require('./pghelper'),
     winston = require('winston');
 
 function findAll(limit) {
-    return db.query('SELECT id, name, description, image__c AS image, productPage__c AS productPage, publishDate__c AS publishDate FROM salesforce.product2 WHERE family=name ORDER BY name ');
+    return db.query('SELECT id, name, family, description, image__c AS image, productPage__c AS productPage, publishDate__c AS publishDate FROM salesforce.product2 WHERE family=name ORDER BY name ');
 };
 
 function findById(id) {
-    return db.query('SELECT id, name, description, image__c AS image, productPage__c AS productPage, publishDate__c AS publishDate FROM salesforce.product2 WHERE id=$1', [id], true);
+    return db.query('SELECT id, name, family, description, image__c AS image, productPage__c AS productPage, publishDate__c AS publishDate FROM salesforce.product2 WHERE id=$1', [id], true);
 };
 
 function getAll(req, res, next) {
