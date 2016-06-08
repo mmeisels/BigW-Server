6554fa537396bf7c6f2324ca4a50c136a87cba4a
Michael Meisels <mmeisels@salesforce.com>
2016-06-09 07:59:42 +1000

    Added Lightning Out Component

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set('port', process.env.PORT || 5000);
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.set('views', __dirname + '/');
 
 app.use(compression());
 app.use(bodyParser({
