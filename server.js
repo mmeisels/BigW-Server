b672bdfdcf57921e0e14ee3db1cae14c7adb016e
Michael Meisels <mmeisels@salesforce.com>
2016-06-09 13:26:46 +1000

    Added Lightning Out Component

--- server.js
+++ server.js
@@ -28,14 +28,7 @@ var express = require('express'),
 
 app.use(cors());
 
-app.all('*', function (req, res, next) {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    //res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-});
-
+app.use(allowCrossDomain);
 
 
 app.set('port', process.env.PORT || 5000);
