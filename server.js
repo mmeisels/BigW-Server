33c7b10eb5b48177cce3da86e00494c5e8622b64
Michael Meisels <mmeisels@salesforce.com>
2016-06-09 13:22:44 +1000

    Added Lightning Out Component

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.all('*', function (req, res, next) {
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    //res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 });
 
 
