91863cb5b4559c8d699a8938587ec0c0014d6e1e
Michael Meisels <mmeisels@salesforce.com>
2016-06-09 11:11:15 +1000

    Added Lightning Out Component

--- server.js
+++ server.js
@@ -35,18 +35,17 @@ var express = require('express'),
          * @type {Object}
          */
         var responseSettings = {
-            "AccessControlAllowOrigin": req.headers.origin,
+            "AccessControlAllowOrigin": true,
             "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
             "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
-            "AccessControlAllowCredentials": true,
-            "Access-Control-Allow-Origin": true
+            "AccessControlAllowCredentials": true
         };
 
         /**
          * Headers
          */
         res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
-        res.header("Access-Control-Allow-Origin",  responseSettings.Access-Control-Allow-Origin);
+        res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
         res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
         res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
 
