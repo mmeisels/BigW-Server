2ef40fbad7829738c07be03fec63e2ff4a238bb5
Michael Meisels <mmeisels@salesforce.com>
2016-06-09 11:51:57 +1000

    Added Lightning Out Component

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var express = require('express'),
          * @type {Object}
          */
         var responseSettings = {
-            "AccessControlAllowOrigin": 'https://bigwdemo.my.salesforce.com',
+            "AccessControlAllowOrigin": true,
             "AccessControlAllowHeaders": "Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
             "AccessControlAllowMethods": "GET",
             "AccessControlAllowCredentials": true
@@ -43,10 +43,10 @@ var express = require('express'),
         /**
          * Headers
          */
-        //res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+        res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
         res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
-        //res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
-        //res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+        res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+        res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
 
         if ('OPTIONS' == req.method) {
             res.send(200);
